b0d849f0b2b3447e180960cd0ec2698320ee95fc
markchanec <markchanec@gmail.com>
2021-01-27 02:11:22 +0800

    Add files via upload

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -1,26 +1,69 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[57]:
 
 
 import streamlit as st
 import plotly.figure_factory as ff
 import numpy as np
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
 
-# Group data together
-hist_data = [x1, x2, x3]
+# In[26]:
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-# Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+st.sidebar.title("Axie Infinity Valuation")
+st.sidebar.header("Virtual market prices")
+selected = st.sidebar.selectbox("Display prices in", ["ETH","USD"])
+tokenID = st.sidebar.number_input("Enter a number", value=82267)
+st.sidebar.button("Random Token ID")
+st.sidebar.write(
+        """<a href="https://www.google.com">[Token Description]</a>""",
+        unsafe_allow_html = True
+)
+st.sidebar.write(
+        """<a href="https://www.insead.edu">[About this project]</a>""",
+        unsafe_allow_html = True
+)
 
-# Plot!
+
+# In[27]:
+
+
+st.subheader("Valuation Range: xx to xx " + selected)
+
+
+# In[71]:
+
+
+np.random.seed(1)
+fig = ff.create_distplot([np.random.randn(1000)], ['KDE curve'], colors=["darkred"], show_hist=False, show_rug=False)
+
+fig.add_shape(type="line",
+    yref= 'paper', y0= 0, y1= 1,
+    xref= 'x', x0= -2, x1= -2,
+    opacity=0.7, line=dict(color="darkred",width=3)
+)
+
+fig.add_shape(type="line",
+    yref= 'paper', y0= 0, y1= 1,
+    xref= 'x', x0= 2, x1= 2,
+    opacity=0.7, line=dict(color="darkred",width=3)
+)
+
 st.plotly_chart(fig, use_container_width=True)
+
+
+# In[29]:
+
+
+st.write(
+        """Notes: This plot produces a valuation range for the entered token ID and shows the distribution of possible valuations with their likelihood represented by the shaded density.
+        Use it to value any token and assess if a token on sale is listed for a reasonable price.
+        The range is a 90% confidence interval.
+        The token ID can be found as the string after the last '/' on the asset's page on OpenSea or the Axie Marketplace.
+        Valuations are generated from multiple machine learning models trained on historical transaction data, incorporating token metadata, cryptocurrency exchange rates, and overall market trends.
+        This app is for informational purposes only and does not constitute financial advice. Last Uptade: Nov. 18, 2020 
+        """
+)
 
